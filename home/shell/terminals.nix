e4979a4cee5645d7bbcec00f7839919ce3a78d53
vgont <vgontdev@gmail.com>
2025-06-08 15:40:41 -0300

    feat (shell): font size

--- home/shell/terminals.nix
+++ home/shell/terminals.nix
@@ -1,13 +1,11 @@
 { ... }:
-let
-  font = "FiraCode Nerd Font Mono";
-in
 {
   programs.ghostty = {
     enable = true;
 
     settings = {
       theme = "GruvboxDark";
+      font-size = 22;
       background-opacity = 0.95;
       background-blur = true;
     };
