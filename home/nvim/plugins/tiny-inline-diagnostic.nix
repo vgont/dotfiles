f9d21401fc8c75421711e0e6839cc326011f4661
vgont <vgontdev@gmail.com>
2025-06-08 15:43:13 -0300

    feat (nixvim.plugins): tiny-inline-diagnostic

--- home/nvim/plugins/tiny-inline-diagnostic.nix
+++ home/nvim/plugins/tiny-inline-diagnostic.nix
@@ -1,22 +1,22 @@
-{ ... }: {
-  programs.nixvim = {
-    diagnostic.settings = { virtual_text = false; };
+{ ... }:
+{
+  programs.nixvim = { 
+    diagnostic.settings = {
+      virtual_text = true;
+      signs = false;
+    };
 
-    plugins.tiny-inline-diagnostic = {
-      enable = true;
+    plugins = {
+      tiny-inline-diagnostic = {
+	enable = true;
 
-      autoLoad = true;
-
-      settings = {
-	multilines = {
-	  enabled = true;
-	};
-	options = {
-	  use_icons_from_diagnostic = true;
-	};
-	preset = "classic";
-	virt_texts = {
-	  priority = 2048;
+	settings = {
+	  multilines = {
+	    enabled = true;
+	  };
+	  options = {
+	    use_icons_from_diagnostic = false;
+	  };
 	};
       };
     };
