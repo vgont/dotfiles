b0671fe250f6a42ce879e224ea559f312cfedc23
vgont <vgontdev@gmail.com>
2025-06-21 14:55:33 -0300

    feat (hosts): maria

---
+++ hosts/maria/home/default.nix
@@ -0,0 +1,67 @@
+{ pkgs, lib, inputs, ... }:
+
+{
+  imports = [
+    inputs.nixvim.homeModules.nixvim
+    ../../../home
+  ];
+
+  home.username = "vgont";
+  home.homeDirectory = "/home/vgont";
+
+  home.activation.createWorkDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -d "$HOME/work" ]; then
+      mkdir -p "$HOME/work"
+    fi
+    if [ ! -d "$HOME/personal-projects" ]; then
+      mkdir -p "$HOME/personal-projects"
+    fi
+  '';
+
+  home.packages = with pkgs; [
+    neofetch
+    macchina
+
+    # apps
+    bitwarden
+    brave
+    vesktop
+
+    # archives
+    zip
+    unzip
+
+    # utils
+    ripgrep
+    bat
+    eza
+    jq
+    fzf
+    yazi
+    zoxide
+    btop
+  ];
+
+  text-editors.nixvim.enable = true;
+
+  window-manager = {
+    niri.enable = true;
+    waybar.enable = true;
+  };
+
+  shell = {
+    fish.enable = true;
+    tmux.enable = true;
+
+    terminals.ghostty.enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "vgont";
+    userEmail = "vgontdev@gmail.com";
+  };
+
+  home.stateVersion = "25.05";
+}
+
---
+++ hosts/maria/nixos/configuration.nix
@@ -0,0 +1,123 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./docker.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "uvcvideo" ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  stylix.enable = true;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+
+  networking.hostName = "maria";
+  networking.networkmanager.enable = true;
+
+  security.polkit.enable = true;
+  security.rtkit.enable = true;
+
+  time.timeZone = "America/Sao_Paulo";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+
+    wireplumber = {
+      enable = true;
+      package = pkgs.wireplumber;
+    };
+  };
+
+  services.libinput.enable = true;
+
+  programs.fish.enable = true;
+  programs.sway.enable = false;
+  programs.niri.enable = true;
+  programs.xwayland.enable = true;
+
+  users.users.vgont = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdeqgzW3tzUvAiqAR3zeP7xntu+S9XZwBbkbjy/DxrC vgontdev@gmail.com"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    pavucontrol
+    swaybg
+  ];
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.lilex
+      nerd-fonts.code-new-roman
+      roboto-serif
+      font-awesome_5
+    ];
+    fontconfig = {
+      defaultFonts = {
+	monospace = [ "Lilex Nerd Font Mono" ];
+	sansSerif = [ "Lilex Nerd Font Mono" ];
+	serif = [ "Lilex Nerd Font Mono" ];
+      };
+    };
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
+  
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "25.05";
+
+}
+
