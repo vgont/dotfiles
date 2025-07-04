7f6320bf106a52dd0f8d7ca4b0c6437979dcb106
vgont <vgontdev@gmail.com>
2025-07-04 10:09:54 -0300

    feat (nixvim.extraConfig): comment some vim cmd

--- home/text-editors/nixvim/extraConfig.lua
+++ home/text-editors/nixvim/extraConfig.lua
@@ -1,14 +1,17 @@
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
+-- vim.cmd [[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+--
+--   highlight Pmenu guibg=none ctermbg=none
+--   highlight PmenuSel guibg=none ctermbg=none
+--   highlight FloatBorder guibg=none ctermbg=none
+--   highlight NormalFloat guibg=none ctermbg=none
+-- ]]
 
-  highlight Pmenu guibg=none ctermbg=none
-  highlight PmenuSel guibg=none ctermbg=none
-  highlight FloatBorder guibg=none ctermbg=none
-  highlight NormalFloat guibg=none ctermbg=none
-]]
+vim.api.nvim_set_hl(0, 'Normal', { bg = "none" })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = "none" })
 
 -- Yank Hightlight
 vim.api.nvim_create_autocmd('TextYankPost', {
