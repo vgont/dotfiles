c6f9e3156d8978c4cc91348402f3647307f76703
vgont <vgontdev@gmail.com>
2025-07-13 18:22:19 -0300

    feat (shell): shell aliases file

---
+++ home/shell/shell-aliases.nix
@@ -0,0 +1,14 @@
+{}: {
+  ls = "eza --icons";
+  ll = "eza --icons -l";
+  cat = "bat";
+  cd = "z";
+  tree = "eza --icons --tree";
+  dd = "cd ~/dotfiles";
+  ww = "cd ~/work";
+  pp = "cd ~/personal-projects/";
+  snorf = "sudo nixos-rebuild switch --flake ~/dotfiles/";
+  ts = "tmux-sessionizer";
+  t = "tmux";
+  vim = "ln -sf ~/dotfiles/home/text-editors/nvim/vgontnvim ~/.config/vgontnvim && NVIM_APPNAME=vgontnvim nvim";
+}
